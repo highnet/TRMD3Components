c193723622df02e7181a5470342200dd56b3dffa
joaquin telleria <joaquintelleria@gmail.com>
2023-07-13 19:30:26 +0200

    updates

--- SnackBars/SnackBar.tsx
+++ SnackBars/SnackBar.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { getPreferredScheme } from "../Gizmos/Themeing";
-import { StringBuilder } from "../Gizmos/StringBuilder";
-import { ISnackBarProps } from "./ISnackBarProps";
+import React, {useState} from "react";
+import {getPreferredScheme} from "../Gizmos/Themeing";
+import {StringBuilder} from "../Gizmos/StringBuilder";
+import {ISnackBarProps} from "./ISnackBarProps";
 import Typography from "../Typography/Typography";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
@@ -14,7 +14,6 @@ const SnackBar: React.FC<ISnackBarProps> = ({
 	onMouseLeave,
 	onMouseMove,
 	onClick,
-	configuration,
 	message,
 	dismissable,
 	action,
@@ -22,7 +21,6 @@ const SnackBar: React.FC<ISnackBarProps> = ({
 }) => {
 	const [_className] = useState(className || "");
 	const [_id] = useState(id || undefined);
-	const [_configuration] = useState(configuration || "one-line");
 	const [_message] = useState(message || "Default message");
 	const [_dismissable] = useState(dismissable || false);
 	const [_actionLabel] = useState(actionLabel || "Action");
@@ -33,7 +31,6 @@ const SnackBar: React.FC<ISnackBarProps> = ({
 	let _computedComponentClassName = new StringBuilder()
 		.add("snackbar")
 		.add("snackbar-" + _theme)
-		.add("snackbar-" + _configuration)
 		.add(_className)
 		.toString();
 
@@ -44,23 +41,23 @@ const SnackBar: React.FC<ISnackBarProps> = ({
 			onMouseEnter={onMouseEnter}
 			onMouseLeave={onMouseLeave}
 			onMouseMove={onMouseMove}
-			onClick={onClick}
-		>
-			<Typography className="message-on-snackbar">{_message}</Typography>
-			<div className="actions-on-snackbar">
-				{action && (
-					<Button
-						onClick={action}
-						className="button-on-snackbar"
-						configuration="text"
-					>
-						{_actionLabel}
-					</Button>
-				)}
+			onClick={onClick}>
+			<div className="snackbar-container">
+				<Typography className="message-on-snackbar">{_message}</Typography>
+				<div className="actions-on-snackbar">
+					{action && (
+						<Button
+							onClick={action}
+							className="button-on-snackbar"
+							configuration="text">
+							{_actionLabel}
+						</Button>
+					)}
 
-				{_dismissable && (
-					<IconButton className="icon-button-on-snackbar">close</IconButton>
-				)}
+					{_dismissable && (
+						<IconButton className="icon-button-on-snackbar">close</IconButton>
+					)}
+				</div>
 			</div>
 		</div>
 	);
