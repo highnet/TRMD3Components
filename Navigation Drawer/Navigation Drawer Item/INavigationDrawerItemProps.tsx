a8cc66c3510013cddcf2ed0d178e1891f7d0217b
Joaquin <joaquintelleria@gmail.com>
2023-07-21 17:43:52 +0200

    docs

--- Navigation Drawer/Navigation Drawer Item/INavigationDrawerItemProps.tsx
+++ Navigation Drawer/Navigation Drawer Item/INavigationDrawerItemProps.tsx
@@ -1,4 +1,15 @@
-import { IComponentProps } from "../../Component/IComponentProps";
+/*
+This code defines an interface called INavigationDrawerItemProps,
+extending IComponentProps. It includes several optional properties to
+configure a navigation drawer item component: 'leadingIcon': A string
+representing the name of the icon to be displayed at the leading position.
+'label': A string representing the label or text to be displayed for the
+navigation drawer item. 'trailingText': A string representing additional
+text to be displayed at the trailing position. 'interactive': A boolean to
+determine if the navigation drawer item is interactive or not.
+*/
+
+import {IComponentProps} from "../../Component/IComponentProps";
 
 export interface INavigationDrawerItemProps extends IComponentProps {
 	leadingIcon?: string;
